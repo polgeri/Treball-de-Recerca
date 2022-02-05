5577c985c11b646369322753e423c4323b1ddb08
Gerard <g.mallofre@gmail.com>
2022-02-05 09:59:45 +0100

    V.1.0.1

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-ScrollReveal().reveal('.showcase', { delay: 500 });
-ScrollReveal().reveal('.news', { delay: 500 });
-ScrollReveal().reveal('.latest-news-body', { delay: 500 });
+ScrollReveal().reveal('.showcase', { delay: 400 });
+ScrollReveal().reveal('.news', { delay: 300 });
+ScrollReveal().reveal('.latest-news-body', { delay: 400 });
+ScrollReveal().reveal('.slide-wrap', { delay: 400 });
+ScrollReveal().reveal('.location', { delay: 300 });
+ScrollReveal().reveal('.header', { delay: 300 });
+ScrollReveal().reveal('.nav-main', { delay: 300 });
+ScrollReveal().reveal('.nav-menu', { delay: 300 });

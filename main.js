7dc68d59ec5dcc494db1ebd05431cf57e2bf1e66
Gerard <g.mallofre@gmail.com>
2022-07-16 13:33:40 +0200

    V. 1.0.3

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-ScrollReveal().reveal('.showcase', { delay: 300 });
-ScrollReveal().reveal('.news', { delay: 200 });
-ScrollReveal().reveal('.latest-news-body', { delay: 200 });
-ScrollReveal().reveal('.slide-wrap', { delay: 300 });
-ScrollReveal().reveal('.location', { delay: 200 });
-ScrollReveal().reveal('.header', { delay: 200 });
-ScrollReveal().reveal('.nav-main', { delay: 200 });
-ScrollReveal().reveal('.nav-menu', { delay: 200 });
+ScrollReveal().reveal('.showcase', { delay: 150 });
+ScrollReveal().reveal('.nav-menu , .header , .nav-main , .location , .slide-wrap , .latest-news-body , .news', { delay: 100 });
+
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+const goUp = document.querySelector("body > div > div.go-up > em");
+
+goUp.addEventListener("click", (_e) => {
+    scrollToTop();
+});

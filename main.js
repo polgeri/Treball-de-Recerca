3f4feb60921c012219e318dfda98409d1ea1dca2
Gerard <g.mallofre@gmail.com>
2022-11-27 19:20:23 +0100

    V. 2.0.1

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+
+/* ESTABLINT FUNCIÓ DE SCROLL-REVEAL */
+
 ScrollReveal().reveal('.showcase', { delay: 150 });
 ScrollReveal().reveal('.nav-menu , .header , .nav-main , .location , .slide-wrap , .latest-news-body , .news , .go-up ', { delay: 100 });
 
+/* FUNCIÓ DEL BOTÓ GO-UP */
 
 function scrollToTop() {
     window.scrollTo({
@@ -9,14 +13,21 @@ function scrollToTop() {
     });
 }
 
+/* DEFININT VARIABLE GO-UP */
+
 const goUp = document.querySelector("body > div > div.go-up > em");
+
+/* APLICANT VARIABLE A LA FUNCIÓ */
 
 goUp.addEventListener("click", () => {
     scrollToTop();
 });
 
+/* DEFININT VARIABLE GO-DOWN(BOTÓ CONTACTES) */
 
 const goDown = document.querySelector("#contactos > strong")
+
+/* DONANT-LI INSTRUCCIONS A LA FUNCIÓ GO-DOWN */
 
 goDown.addEventListener("click", () => {
     window.scrollTo({
